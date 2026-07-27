d0916a0a1601c438ea3d7ba1c15662b866ef2ea2
Altamash Siddiqui <siddiquialtamash08@gmail.com>
2026-07-27 21:32:34 +0530

    Day 2: Added welcome and user input

---
+++ backend/app.py
@@ -0,0 +1,11 @@
+def welcome():
+    print("================================")
+    print("   AI Career Copilot")
+    print("================================")
+
+def get_user_name():
+    name = input("Enter your name: ")
+    print(f"Welcome, {name}!")
+
+welcome()
+get_user_name()
